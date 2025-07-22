4ccd138a1445fed540c0f025dbd6a927ef2db37d
AMIT9437 <amitsamal9437@gmail.com>
2025-07-23 00:07:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import ServiceDetails from "./pages/ServiceDetails";
@@ -14,23 +15,27 @@ import Signup from "./pages/Signup";
 function App() {
   return (
     <Router>
-      <div style={{ display: "flex", minHeight: "100vh" }}>
-        <Sidebar />
-        <div style={{ flex: 1, padding: "1rem", background: "#f7f7fa" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/services/:serviceId" element={<ServiceDetails />} />
-            <Route path="/price-details" element={<PriceDetails />} />
-            <Route path="/documents" element={<Documents />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
+      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        <Header />
+        <div style={{ display: "flex", flex: 1 }}>
+          <Sidebar />
+          <div style={{ flex: 1, padding: "1rem", background: "#f7f7fa" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/services/:serviceId" element={<ServiceDetails />} />
+              <Route path="/price-details" element={<PriceDetails />} />
+              <Route path="/documents" element={<Documents />} />
+              <Route path="/contact-us" element={<ContactUs />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
   );
 }
+
 export default App;
