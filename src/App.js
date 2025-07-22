e7965000ca20ebf5823892a1c0225772ee762cc6
AMIT9437 <amitsamal9437@gmail.com>
2025-07-23 00:24:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
